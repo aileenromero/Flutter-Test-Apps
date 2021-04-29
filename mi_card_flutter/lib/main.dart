ef4ce8142df546b21174e054973db1197a39cffe
Aileen B. Romero <devaileenromero@gmail.com>
2021-04-29 16:38:59 +0800

    - update micard image - finished micard personal business card app

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,17 +13,19 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
                 backgroundImage: AssetImage('images/developer.png'),
+                backgroundColor: Colors.white,
               ),
               Text(
                 'Aileen Romero',
                 style: TextStyle(
                     fontFamily: 'Pacifico',
-                    fontSize: 20.0,
-                    color: Colors.teal.shade100,
+                    fontSize: 30.0,
+                    color: Colors.white,
                     fontWeight: FontWeight.bold,
                     letterSpacing: 2.5),
               ),
@@ -30,18 +33,48 @@ class MyApp extends StatelessWidget {
                 'FLUTTER DEVELOPER',
                 style: TextStyle(
                     fontFamily: 'SourceSansPro',
-                    fontSize: 20.0,
+                    fontSize: 15.0,
                     color: Colors.teal.shade100,
                     fontWeight: FontWeight.bold,
                     letterSpacing: 2.5),
               ),
-              // Container(
-              //   child: Row(
-              //     children: <Widget>[
-              //
-              //     ],
-              //   ),
-              // ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(Icons.phone, color: Colors.teal),
+                    title: Text(
+                      '+63 912 345 6789',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 15.0),
+                    ),
+                  )),
+              Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'aileenromero@email.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 15.0,
+                      ),
+                    ),
+                  ))
             ],
           ),
         ),
