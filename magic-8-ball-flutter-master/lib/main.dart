218cb32107b715d998a877a6d194da80503cb8a7
Aileen B. Romero <devaileenromero@gmail.com>
2021-04-29 23:57:13 +0800

    Magic 8 Ball - add flatbutton on image and print (I got clicked)

--- magic-8-ball-flutter-master/lib/main.dart
+++ magic-8-ball-flutter-master/lib/main.dart
@@ -29,7 +29,11 @@ class _BallState extends State<Ball> {
   @override
   Widget build(BuildContext context) {
     return Center(
+        child: FlatButton(
+      onPressed: () {
+        print('I got clicked');
+      },
       child: Image.asset('images/ball1.png'),
-    );
+    ));
   }
 }
