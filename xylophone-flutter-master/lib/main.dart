df7d73b7e7590b25209795fc9f9e71adad4c11e2
Aileen B. Romero <devaileenromero@gmail.com>
2021-06-07 22:32:54 +0800

    --

--- xylophone-flutter-master/lib/main.dart
+++ xylophone-flutter-master/lib/main.dart
@@ -8,7 +8,11 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+              child: TextButton(
+            onPressed: () {},
+            child: Text('Click Me'),
+          )),
         ),
       ),
     );
